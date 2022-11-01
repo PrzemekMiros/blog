9ef4b2a4ceba459bc36f59f80c0ae3dc381a0183
PrzemekMiros <kontakt@futurewebstudio.pl>
2022-11-01 16:07:34 +0100

    Add style, scripts and img source

--- .eleventy.js
+++ .eleventy.js
@@ -1,5 +1,9 @@
 module.exports = function(eleventyConfig) {
 
+    eleventyConfig.addPassthroughCopy("./src/css");
+    eleventyConfig.addPassthroughCopy("./src/js");
+    eleventyConfig.addPassthroughCopy("./src/images");
+
     return {
       dir: {
         input: "src",
