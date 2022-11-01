7f76e5c75ddb18ecffeba28a9c119be95b11439a
PrzemekMiros <kontakt@futurewebstudio.pl>
2022-11-02 00:20:43 +0100

    Add description field

--- .eleventy.js
+++ .eleventy.js
@@ -1,3 +1,5 @@
+const { DateTime } = require("luxon");
+
 module.exports = function(eleventyConfig) {
 
     eleventyConfig.addPassthroughCopy("./src/assets/css");
@@ -6,6 +8,20 @@ module.exports = function(eleventyConfig) {
     eleventyConfig.addPassthroughCopy("./src/assets/fonts");
     eleventyConfig.addPassthroughCopy("./src/assets/webfonts");
     eleventyConfig.addPassthroughCopy('./src/admin');
+
+    eleventyConfig.addFilter("readablePostDate", (dateObj) => {
+      return DateTime.fromJSDate(dateObj, {
+          zone: "Europe/Amsterdam",
+      }).setLocale('pl').toLocaleString(DateTime.DATE_FULL);
+  });
+  
+  eleventyConfig.addFilter("postDate", (dateObj) => {
+      return DateTime.fromJSDate(dateObj, {
+          zone: "Europe/Amsterdam",
+      }).setLocale('pl').toISODate();
+  });
+
+    eleventyConfig.addShortcode("year", () => `${new Date().getFullYear()}`);
 
     return {
       dir: {
